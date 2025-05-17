a12f41e38f81e2f2dfe0cf17fb2e941672e31e02
EranMiz16 <eranmiz16@gmail.com>
2025-05-17 13:11:48 +0300

    H

--- src/components/pages/HomePage/Search/Search.jsx
+++ src/components/pages/HomePage/Search/Search.jsx
@@ -5,22 +5,18 @@ import './Search.css';
 
 export const SearchBar = () => {
   return (
-    
+
     <div className="search-wrapper">
-      <div className="search-container">
-        <input type="text" placeholder="Search" className="search-input" />
-        <FaSearch className="search-icon" />
-      </div>
+        <div className="search-container">
+          <input type="text" placeholder="Search" className="search-input" />
+          <FaSearch className="search-icon" />
+        </div>
 
       <div className="search-options">
-        <label className="preference-checkbox">
-          <input type="checkbox" />
-          Show by Preferences
-        </label>
-
-        <FilterComponent />
+          <FilterComponent />
       </div>
     </div>
+
   );
 };
 
