8ca62bc2bb97d861826fba75f751d731b50ffad6
nivbn <nivbnr@gmail.com>
2025-05-17 13:42:07 +0300

    sds

--- src/components/pages/MyProfilePage/ProfileItemsSection/ProfileItemsSection.jsx
+++ src/components/pages/MyProfilePage/ProfileItemsSection/ProfileItemsSection.jsx
@@ -35,7 +35,7 @@ const ProfileItemsSection = ({ onAddItem, onItemClick }) => {
   return (
     <div className="profile-items-section">
       <div className="section-header">
-        <h2 className="section-title">Ready to Swap</h2>
+        <h2 className="section-title">Ready to Give</h2>
         <button className="add-item-button" onClick={onAddItem}>
           <img src="/icons/plus-square.svg" alt="Add item" />
         </button>
